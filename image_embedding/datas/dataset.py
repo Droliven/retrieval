a8f517ffd589572db08889e2cd1f540b3ffdc94f
liuzhian <csliuzhian@mail.scut.edu.cn>
2021-06-21 21:05:17 +0800

    CN_insta_50K数据集,多卡训练,支持resnet50

--- image_embedding/datas/dataset.py
+++ image_embedding/datas/dataset.py
@@ -8,6 +8,7 @@
 @ide     : PyCharm
 @time    : 2021-05-26 19:21
 '''
+import os.path as osp
 from tqdm import tqdm
 from torch.utils.data import Dataset
 import os
@@ -15,12 +16,15 @@ from PIL import Image
 from torchvision import transforms
 import numpy as np
 
+
 class ImgDataset(Dataset):
-    def __init__(self, img_path_prefix=r"F:\model_report_data\multimodal_image_retrieval\InstaCities1M\img_resized_1M\cities_instagram", txt_embed_prefix="", path=r"./test_path.txt"):
+    def __init__(self, ds_root, split="train"):
         super(ImgDataset, self).__init__()
 
-        self.img_path_prefix = img_path_prefix
-        self.txt_embed_prefix = txt_embed_prefix
+        self.ds_root = ds_root
+        assert split in ["train", "val", "test"]
+        self.split = split
+        self.cities = ["beijing", "shanghai", "guangzhou", "hangzhou"]
 
         self.preprocess = transforms.Compose([
             transforms.Resize(256),
@@ -29,34 +33,48 @@ class ImgDataset(Dataset):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
-        with open(path, "r", encoding='utf-8') as f:
-            self.path = f.read().splitlines()
+        self.sample_ids = []
 
+        for city in self.cities:
+            with open(osp.join(ds_root, city, "%s.txt" % split), "r", encoding='utf-8') as f:
+                city_ids = f.read().splitlines()
+                # 添加城市前缀
+                city_ids = [osp.join(city, id) for id in city_ids]
+
+                self.sample_ids.extend(city_ids)
 
     def __len__(self):
-        return len(self.path)
+        return len(self.sample_ids)
 
-    def __getitem__(self, item):
+    def __getitem__(self, idx):
         # print(f"{item}-{self.img_path[item]}")
+        sample_id = self.sample_ids[idx]
+        city = sample_id.split("/")[0]
 
-        img = Image.open(os.path.join(self.img_path_prefix, self.path[item] + '.jpg')).convert('RGB')  # wh = 1546, 1213
+        img_path = osp.join(self.ds_root, sample_id + ".jpg")
+        img_path = img_path.replace(city, osp.join(city, "img")) # 加上img文件夹前缀
+
+        txt_feat_path = osp.join(self.ds_root, sample_id + ".npy")
+        txt_feat_path = txt_feat_path.replace(city, osp.join(city, "w2v_feat"))
+
+        img = Image.open(img_path).convert('RGB')  # wh = 1546, 1213
         if len(np.array(img).shape) == 2:
             img = np.stack([np.array(img), ] * 3, axis=-1)
             img = Image.fromarray(img)
 
         img = self.preprocess(img)
-        txt_embed = np.load(os.path.join(self.txt_embed_prefix, self.path[item] + '.npy'))
+        txt_embed = np.load(txt_feat_path)
         return img, txt_embed
 
 
 if __name__ == "__main__":
     from torch.utils.data import DataLoader
-    dset = ImgDataset()
+
+    dset = ImgDataset(ds_root=r"/home/liuzhian/hdd4T/datasets/CN_insta_50K")
     dl = DataLoader(dset, batch_size=2)
 
-    for idx,(img, txt_emb) in tqdm(enumerate(dl)):
-        # print(idx)
-        # print(img)
-        # print(txt_emb)
+    for idx, (img, txt_emb) in tqdm(enumerate(dl)):
+        print(idx)
+        print(img)
+        print(txt_emb)
         pass
-
--- image_embedding/runner.py
+++ image_embedding/runner.py
@@ -8,8 +8,10 @@
 @ide     : PyCharm
 @time    : 2021-05-26 15:56
 '''
+from tqdm import tqdm
+import json
 import os
-
+import os.path as osp
 import torch
 import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
@@ -18,15 +20,27 @@ from image_embedding.datas.dataset import ImgDataset
 from image_embedding.config import Config
 from torch.utils.data import DataLoader
 
-def embedding_loss(pred, gt):
+LOG_EVERY = 100
+
+
+class EmbeddingLoss(nn.Module):
     '''
 
     :param pred: B, D
     :param gt:
     :return:
     '''
-    loss = -torch.mean(torch.mul(gt, torch.log(pred)) + torch.mul((1-gt), torch.log(1-pred)))
-    return loss
+
+    def __init__(self):
+        super(EmbeddingLoss, self).__init__()
+
+    def forward(self, pred, gt):
+        pred = torch.sigmoid(pred)
+        gt = torch.sigmoid(gt)
+
+        loss = -torch.mean(torch.mul(gt, torch.log(pred)) + torch.mul((1 - gt), torch.log(1 - pred)))
+        return loss
+
 
 def lr_decay(optimizer, lr_now, gamma):
     lr = lr_now * gamma
@@ -34,7 +48,8 @@ def lr_decay(optimizer, lr_now, gamma):
         param_group['lr'] = lr
     return lr
 
-class Runner():
+
+class Runner:
     def __init__(self):
         self.cfg = Config()
         self.best_eval_loss = 1e8
@@ -43,18 +58,27 @@ class Runner():
         self.lr = self.cfg.lr
 
         self.writter = SummaryWriter(log_dir=self.cfg.ckpt_dir)
+        with open(osp.join(self.cfg.ckpt_dir, "config.json"), "w") as fp:
+            fp.write(json.dumps(self.cfg.__dict__))
 
-        self.model = Embedding(self.cfg.embedding_dim)
+        self.model = Embedding(self.cfg.embedding_dim, self.cfg.backbone_type)
+        self.losser = EmbeddingLoss()
+
         if self.cfg.device != "cpu":
-            self.model = self.model.to(self.cfg.device)
+            self.model = nn.DataParallel(self.model).to(self.cfg.device)
+            self.losser = nn.DataParallel(self.losser).to(self.cfg.device)
 
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.cfg.lr, momentum=self.cfg.sgd_momentum)
 
-        self.train_set = ImgDataset(self.cfg.img_base_dir, self.cfg.txt_embedding_base_dir, self.cfg.train_path)
-        self.valid_set = ImgDataset(self.cfg.img_base_dir, self.cfg.txt_embedding_base_dir, self.cfg.valid_path)
+        self.train_set = ImgDataset(self.cfg.ds_root, split="train")
+        self.valid_set = ImgDataset(self.cfg.ds_root, split="val")
         # self.test_set = ImgDataset(self.cfg.img_base_dir, self.cfg.txt_embedding_base_dir, self.cfg.test_path)
-        self.train_dataloader = DataLoader(self.train_set, batch_size=self.cfg.train_batch_size, num_workers=self.cfg.num_workers, shuffle=True, pin_memory=True, drop_last=True)
-        self.valid_dataloader = DataLoader(self.valid_set, batch_size=self.cfg.valid_batch_size, num_workers=self.cfg.num_workers, shuffle=False, pin_memory=True, drop_last=True)
+        self.train_dataloader = DataLoader(self.train_set, batch_size=self.cfg.train_batch_size,
+                                           num_workers=self.cfg.num_workers, shuffle=True, pin_memory=True,
+                                           drop_last=True)
+        self.valid_dataloader = DataLoader(self.valid_set, batch_size=self.cfg.valid_batch_size,
+                                           num_workers=self.cfg.num_workers, shuffle=False, pin_memory=True,
+                                           drop_last=True)
         # self.test_dataloader = DataLoader(self.test_set, batch_size=self.cfg.test_batch_size, num_workers=self.cfg.num_workers, shuffle=False, pin_memory=True, drop_last=True)
 
         print(f"train len: {self.train_set.__len__()}")
@@ -65,60 +89,64 @@ class Runner():
         for epo in range(1, self.cfg.epochs + 1):
             # train
             self.model.train()
-            for img, txt_emb in self.train_dataloader:
+            for img, txt_emb in tqdm(self.train_dataloader, total=len(self.train_dataloader)):
                 if self.cfg.device != "cpu":
                     img = img.float().to(self.cfg.device)
                     txt_emb = txt_emb.float().to(self.cfg.device)
 
                 out_emb = self.model(img)
-                out_emb = nn.Sigmoid()(out_emb)
-                txt_emb = nn.Sigmoid()(txt_emb)
-
-                loss = embedding_loss(out_emb, txt_emb)
+                loss = self.losser(out_emb, txt_emb)
 
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
                 loss = loss.cpu().data.numpy()
-                # TODO summary
-                self.writter.add_scalar('Loss/train', loss, self.global_step)
                 self.global_step += 1
 
-            if epo % self.cfg.eval_iters_evary == 0 or epo == self.cfg.epochs:
-                # eval
-                self.model.eval()
-                avg_eval_loss = 0
-                for idx, (img, txt_emb) in enumerate(self.valid_dataloader):
-                    if self.cfg.device != "cpu":
-                        img = img.float().to(self.cfg.device)
-                        txt_emb = txt_emb.float().to(self.cfg.device)
+                # TODO summary
+                if self.global_step % LOG_EVERY == 0:
+                    self.writter.add_scalar('Loss/train', loss, self.global_step)
 
-                    with torch.no_grad():
-                        out_emb = self.model(img)
-                        out_emb = nn.Sigmoid()(out_emb)
-                        txt_emb = nn.Sigmoid()(txt_emb)
+            # 每轮结束 forward  validation set
+            self.model.eval()
+            avg_eval_loss = 0
+            for idx, (img, txt_emb) in enumerate(tqdm(self.valid_dataloader, total=len(self.valid_dataloader))):
+                if self.cfg.device != "cpu":
+                    img = img.float().to(self.cfg.device)
+                    txt_emb = txt_emb.float().to(self.cfg.device)
 
-                        loss_eval = embedding_loss(out_emb, txt_emb)
-                        avg_eval_loss += loss_eval.cpu().data.numpy()
+                with torch.no_grad():
+                    out_emb = self.model(img)
 
-                avg_eval_loss = avg_eval_loss / (idx)
-                self.writter.add_scalar('Loss/val', avg_eval_loss, epo)
+                    loss_eval = self.losser(out_emb, txt_emb)
+                    avg_eval_loss += loss_eval.cpu().data.numpy()
 
-                if avg_eval_loss < self.best_eval_loss:
-                    self.best_eval_loss = avg_eval_loss
+            avg_eval_loss = avg_eval_loss / idx
+            self.writter.add_scalar('Loss/val', avg_eval_loss, epo)
 
-                print(f">>> epoch {epo} / {self.cfg.epochs}, lr: {self.lr}, avg_eval_loss: {avg_eval_loss}, best_eval_loss: {self.best_eval_loss}")
-                self.save(epo, self.best_eval_loss, avg_eval_loss)
+            if avg_eval_loss < self.best_eval_loss:
+                self.best_eval_loss = avg_eval_loss
+                print(f">>> epoch {epo} / {self.cfg.epochs}, lr: {self.lr}, "
+                      f"avg_eval_loss: {avg_eval_loss},"
+                      f" best_eval_loss: {self.best_eval_loss}")
+                self.save(epo, self.best_eval_loss, avg_eval_loss, is_best=True)
 
             if epo % self.cfg.lr_descent_every == 0:
                 # descent lr
                 self.lr = lr_decay(self.optimizer, self.lr, self.cfg.lr_descent_rate)
 
+        self.save(self.cfg.epochs, -1, -1, is_best=False)
 
-    def save(self, epoch, best_loss, curr_loss):
+    def save(self, epoch, best_loss, curr_loss, is_best=False):
         print(f"Saved epoch: {epoch}, currloss: {curr_loss}, bestloss: {best_loss} to {self.cfg.ckpt_dir}.")
+        save_name = f"epoch{epoch}_currloss{curr_loss}_bestloss{best_loss}.pth.tar" if not is_best \
+            else "best.pth.tar"
         torch.save({'epoch': epoch, 'state_dict': self.model.state_dict(), 'best_loss': best_loss,
                     'optimizer': self.optimizer.state_dict(), 'curr_loss': curr_loss},
-                   os.path.join(self.cfg.ckpt_dir, f"epoch{epoch}_currloss{curr_loss}_bestloss{best_loss}.pth.tar"))
+                   os.path.join(self.cfg.ckpt_dir, save_name))
 
+
+if __name__ == '__main__':
+    runner = Runner()
+    runner.run()
